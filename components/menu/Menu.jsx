8a00759d6c3b2e3428b9e7322dead4768a32ca7c
Mirco Bertelli <mircobe87@gmail.com>
2023-07-22 20:01:09 +0200

    burger menu for mobiles

--- components/menu/Menu.jsx
+++ components/menu/Menu.jsx
@@ -1,9 +1,30 @@
 import styles from './menu.module.css';
+import clsx from 'clsx';
+
+import { GrMenu } from 'react-icons/gr';
+import { useState } from 'react';
 
 export default function Menu({children}) {
+    const [ open, setOpen ] = useState(false);
+
+    function toggleMenu() {
+        console.log("ciao");
+        setOpen(!open);
+    }
+
+    let containerClasses = clsx({
+        [styles.container] : true,
+        [styles.open] : open
+    });
+
     return (
-        <div className={styles.container}>
-            {children}
-        </div>
+        <>
+            <button className={styles.burger} onClick={toggleMenu}>
+                <GrMenu></GrMenu>
+            </button>
+            <div className={containerClasses}>
+                {children}
+            </div>
+        </>
     );
 }
