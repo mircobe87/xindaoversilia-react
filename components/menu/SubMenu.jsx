bc7f61c95e85d574aa614758068f0f176cf310bd
Mirco Bertelli <mircobe87@gmail.com>
2023-07-26 21:27:20 +0200

    fix menu style for both mobile and desktop screens

--- components/menu/SubMenu.jsx
+++ components/menu/SubMenu.jsx
@@ -1,17 +1,17 @@
 import styles from './submenu.module.css';
 import clsx from 'clsx';
 
-import { BsCaretDown, BsCaretUp } from 'react-icons/bs';
+import { BsChevronDown, BsChevronUp } from 'react-icons/bs';
 import { useState } from 'react';
 
 export default function SubMenu({name, children}) {
     const [open, setOpen] = useState(false);
 
     return (
-        <div className={styles.container} onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)} >
-            <div className={styles.containerTitle}>
-                {open ? <BsCaretDown/> : <BsCaretUp/>}
+        <div className={styles.container} >
+            <div className={styles.containerTitle} onClick={() => setOpen(!open)}>
                 <div>{name}</div>
+                {open ? <BsChevronUp/> : <BsChevronDown/>}
             </div>
             <div className={clsx(styles.containerList, {[styles.visible]: open})}>{children}</div>
         </div>
